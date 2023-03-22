7ce0948a73e891207d986f5e8c767874f0092ea6
Aja Singer <ajasinger@gmail.com>
2023-03-22 11:21:44 -0400

    display JSON results

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@ buttonRepo.addEventListener("click",getRepo);
 
 async function getRepo() {
     // fetch from github API 
+    //change this link 
     const url = "https://api.github.com/search/repositories?q=stars:>100000";
     const response = await fetch(url);
 
     //display results 
     const result = await response.json();
     result.items.forEach(i => {
-        
+        //use innerHTML instead 
+        results.appendChild(document.createTextNode(i.full_name));
     });
 
     console.log(result);
