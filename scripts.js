2d1fdf0b0ca886aeb48db1485ea03f269fa0868f
Aja Singer <ajasinger@gmail.com>
2023-03-22 12:09:34 -0400

    style JSON response

--- scripts.js
+++ scripts.js
@@ -10,8 +10,19 @@ async function getRepo() {
     //display results 
     const result = await response.json();
     result.items.forEach(i => {
-        //use innerHTML instead 
-        results.appendChild(document.createTextNode(i.full_name));
+        //create new link element called anchor
+        const anchor = document.createElement("a");
+        //linked pulled from github API
+        anchor.href = i.html_url;
+        //this will be the link text 
+        anchor.textContent = i.full_name;
+        //adds anchor to element with divResults id
+        divResult.appendChild(anchor)
+        //add line br between links
+        divResult.appendChild(document.createElement("br"));
+
+        //this displays results with no styling (how to use innerHTML instead?) 
+        //results.appendChild(document.createTextNode(i.full_name));
     });
 
     console.log(result);
